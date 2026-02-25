ffb0bb80aedf2d1f131eed91e7bf7511d44be88c
Jose Loarca <joseloarca97@icloud.com>
2026-02-25 15:28:20 -0500

    Added S3 states

--- lib/aws-automation-stack.ts
+++ lib/aws-automation-stack.ts
@@ -1,20 +1,55 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-import {Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {PolicyDocument, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 import {DefinitionBody, StateMachine} from "aws-cdk-lib/aws-stepfunctions";
+import {Bucket} from "aws-cdk-lib/aws-s3";
+import {CfnOutput, RemovalPolicy} from "aws-cdk-lib/core";
 
 export class AwsAutomationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const stateMachineRole = new Role(this, 'StateMachineAIRole', {
-      assumedBy: new ServicePrincipal('states.amazonaws.com'),
+    // -- S3 --
+    const dataBucket = new Bucket(this, 'MyStepFunctionsDataBucket', {
+      bucketName: 'my-step-functions-s3-data-bucket',
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
     });
 
+    const s3AccessPolicy = new PolicyDocument({
+      statements: [
+          new PolicyStatement({
+            actions: ['s3:GetObject', 's3:PutObject'],
+            resources: [
+                dataBucket.bucketArn,
+                `${dataBucket.bucketArn}/*`
+            ],
+          }),
+      ],
+    });
+
+    // -- Step Functions Role --
+    const stateMachineRole = new Role(this, 'StateMachineAIRole', {
+      assumedBy: new ServicePrincipal('states.amazonaws.com'),
+      inlinePolicies: {
+        S3AccessPolicy: s3AccessPolicy
+      }
+    });
+
+    // -- Step Functions Workflow --
     const workflow = new StateMachine(this, 'MyStepFunctionsWorkflow', {
       stateMachineName: 'MyStepFunctionsWorkflow',
       role: stateMachineRole,
-      definitionBody: DefinitionBody.fromFile('statemachine/definition.asl.json')
-    })
+      definitionBody: DefinitionBody.fromFile('statemachine/definition.asl.json'),
+      definitionSubstitutions: {
+        DataBucketName: dataBucket.bucketName,
+      }
+    });
+
+    // -- CloudFormation Output --
+    new CfnOutput(this, 'CFOutputStateMachineArn', {
+      value: workflow.stateMachineArn
+    });
+
   }
 }
